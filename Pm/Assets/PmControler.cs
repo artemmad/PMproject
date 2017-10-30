8f0e0d2a3ead4b492f593d8798320d84a109671a
Артем Макаревич <artemmakarevich1997@gmail.com>
2017-10-30 13:38:40 +0300

    Start

---
+++ Pm/Assets/PmControler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PmControler : MonoBehaviour {
+
+
+    public GameObject korob;
+    public GameObject predohran;
+    public GameObject mag;
+    public GameObject scobe;
+    public GameObject spring;
+    private int stage = 0;
+    
+
+
+
+    public void setgo()
+    {
+        stage++;
+        switch(stage){
+            case 1:
+                {
+                    mag.GetComponent<EasyTransform>().isActive = false;
+                    predohran.GetComponent<EasyTransform>().isActive = true;
+                    break;
+                }
+            case 2:
+                {
+                    predohran.GetComponent<EasyTransform>().isActive = false;
+                    korob.GetComponent<EasyTransform>().isActive = true;
+                    break;
+                }
+
+            default:
+                stage = 0;
+                break;
+        }
+
+    }
+    // Use this for initialization
+    void Start () {
+        mag.GetComponent<EasyTransform>().isActive = true;
+        predohran.GetComponent<EasyTransform>().isActive = false;
+        scobe.GetComponent<EasyTransform>().isActive = false;
+        spring.GetComponent<EasyTransform>().isActive = false;
+        korob.GetComponent<EasyTransform>().isActive = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
